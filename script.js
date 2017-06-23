8701375ce9b10e3c888e78478b03263da8028874
michael <michael@rosewoodinitiative.org>
2017-06-23 15:27:01 -0400

    second change

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 var $textInput = $("#textInput"),
-      $button = $("#doAction"),
-      $display = $("#display");
-      
-      $button.on("click", function(){
-        let word = $textInput.val();
-        let reversedWord = word.split("").reverse();
-        
-        $display.text(reversedWord.join(""));
-      });  
+    $button = $("#doAction"),
+    $display = $("#display"),
+    $options = $("input[type=radio]"),
+    method = "reverse";
+
+$options.on("click", function(){
+  method = $(this).val();
+});
+
+$button.on("click", function() {
+  
+  if (method === "reverse") {
+    let word = $textInput.val();
+    let reversedWord = word.split("").reverse().join("");  
+    $display.text(reversedWord);  
+  }
+  
+  if (method === "random") {
+    let word = $textInput.val();
+    let randomizedWord = word.split("").sort(function(a, b) {
+      return 0.5 - Math.random();
+    });
+    $display.text(randomizedWord.join(""));
+  }
+  
+  if (method === "alphabetize") {
+    let word = $textInput.val();
+    let alphabetizedWord = word.split("").sort().join("");  
+    $display.text(alphabetizedWord);  
+  }
+  
+});
